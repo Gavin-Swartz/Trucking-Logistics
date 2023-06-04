e441f1c108288b40b5f52c7f9e2284d7a3c67306
Gavin-Swartz <gvndswartz@gmail.com>
2023-06-04 18:06:05 -0500

    Formatting, removed unnecessary methods.

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 program_name = 'Swartz Trucking Logistics'
 
 
-def prepare_data():
-    print('Preparing data, please wait...\n')
-
-
 def intro_menu():
     print('Welcome to', program_name)
     print('Optimize your trucking services in Nebraska, Iowa, South Dakota, Kansas, and Missouri.\n')
@@ -14,6 +10,5 @@ def add_shipments():
     pass
 
 
-prepare_data()
 intro_menu()
 add_shipments()
