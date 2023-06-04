92e479f348f47574faed3477c20c4955412ba04a
Gavin-Swartz <gvndswartz@gmail.com>
2023-06-04 13:57:52 -0500

    Added necessary methods, no functionality

---
+++ main.py
@@ -0,0 +1,19 @@
+program_name = 'Swartz Trucking Logistics'
+
+
+def prepare_data():
+    print('Preparing data, please wait...\n')
+
+
+def intro_menu():
+    print('Welcome to', program_name)
+    print('Optimize your trucking services in Nebraska, Iowa, South Dakota, Kansas, and Missouri.\n')
+
+
+def add_shipments():
+    pass
+
+
+prepare_data()
+intro_menu()
+add_shipments()
