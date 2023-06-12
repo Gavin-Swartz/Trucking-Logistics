ea6b4b84912609754b56abec46c1c4a9f0335a42
Gavin-Swartz <gvndswartz@gmail.com>
2023-06-12 12:51:41 -0500

    Added break to end while loop, did not fix the issue with finding routes further up on the list.

--- get_shipment_info.py
+++ get_shipment_info.py
@@ -8,23 +8,24 @@ with open('shipments.csv') as shipments_file:
     csv_reader = csv.reader(shipments_file, delimiter=',')
     top_line = True
 
-    for row in csv_reader:
+    for rows in csv_reader:
         if top_line:
             top_line = False
         else:
-            start_locations.append(row[0])
-            end_locations.append(row[1])
+            start_locations.append(rows[0])
+            end_locations.append(rows[1])
 
 # Get distances of each shipment
 with open('distances.csv') as distances_file:
     distances_csv_reader = csv.reader(distances_file, delimiter=',')
     top_line = True
 
-    route_stretches_index = 0
-    for row in distances_csv_reader:
-        if top_line:
-            top_line = False
-        else:
-            if start_locations[route_stretches_index] in row[0] and end_locations[route_stretches_index] in row[1]:
-                print(row[0], 'to', row[1], 'is', row[2], 'miles')
+    route_stretches_index = 0   # Increases to match index of the route stretch
+    while route_stretches_index < len(start_locations):
+        for rows in distances_csv_reader:
+            if top_line:
+                top_line = False
+            elif start_locations[route_stretches_index] in rows[0] and end_locations[route_stretches_index] in rows[1]:
+                print(rows[0], 'to', rows[1], 'is', rows[2], 'miles')
                 route_stretches_index += 1
+        break
