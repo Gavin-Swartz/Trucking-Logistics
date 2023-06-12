9c5aed3422d780563ea72dfb79ed3a8a545b0c38
Gavin-Swartz <gvndswartz@gmail.com>
2023-06-12 13:38:29 -0500

    Fixed route finding issue, csv reader now restarts for every iteration of loop.

--- get_shipment_info.py
+++ get_shipment_info.py
@@ -16,13 +16,12 @@ with open('shipments.csv') as shipments_file:
             end_locations.append(rows[1])
 
 # Get distances of each shipment
-with open('distances.csv') as distances_file:
-    distances_csv_reader = csv.reader(distances_file, delimiter=',')
-    top_line = True
-
-    route_stretches_index = 0   # Increases to match index of the route stretch
-    while route_stretches_index < len(start_locations):
-        for routes in start_locations:
+top_line = True
+route_stretches_index = 0  # Increases to match index of the route stretch
+while route_stretches_index < len(start_locations):
+    for routes in start_locations:
+        with open('distances.csv') as distances_file:
+            distances_csv_reader = csv.reader(distances_file, delimiter=',')
             for rows in distances_csv_reader:
                 if top_line:
                     top_line = False
@@ -31,4 +30,3 @@ with open('distances.csv') as distances_file:
                     print(rows[0], 'to', rows[1], 'is', rows[2], 'miles')
                     route_stretches_index += 1
                     break
-        break
